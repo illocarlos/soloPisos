59c8310597844020e38c77a1775a89a61d18d4ad
illocarlos <carlosalbendiz93@gmail.com>
2024-01-08 21:40:16 +0100

    create form in admincreate

--- src/views/admin/AdminView.vue
+++ src/views/admin/AdminView.vue
@@ -4,4 +4,11 @@
 
 <template>
     <h2 class="text-center text-h3 my-5 font-weight-bold">ADMIN PANEL</h2>
+    <v-btn
+    color="grey"
+    variant="flat"
+    :to="{name:'admin-create'}"
+    >
+        add propertie
+    </v-btn>
 </template>
--- src/views/admin/CreateView.vue
+++ src/views/admin/CreateView.vue
@@ -1,7 +1,87 @@
 <script setup>
+const countRooms = [1, 2, 3, 4, 5, 6, 7, 8];
 
 </script>
 
 <template>
-    <h2 class="text-center text-h3 my-5 font-weight-bold">CREATE</h2>
+    <v-card
+    max-width="800"
+    flat
+    class="mx-auto">
+<v-card-title class="text-center text-h4 font-weight-bold "
+tag="h2">
+    create a propertie
+</v-card-title>
+  <v-form class="mt-10">
+<v-text-field
+class="mb-5"
+label="Title of propertie"/>
+
+<v-file-input
+accept="image/jpeg"
+label="photo"
+prepend-icon="mdi-camera"
+class="mb-5"
+/>
+    <v-text-field
+    class="mb-5"
+    label="Price"/>
+
+
+    <v-row>
+<v-col
+cols="12"
+md="4"
+>
+    <v-select
+    class="mb-5"
+    label="available rooms"
+    :items="countRooms"/>
+</v-col>
+<v-col
+    cols="12"
+    md="4"
+    >
+      <v-select
+        class="mb-5"
+        label="WC"
+        :items="countRooms"/>
+        </v-col>
+        <v-col
+        cols="12"
+    md="4">
+
+          <v-select
+        class="mb-5"
+        label="available rooms"
+        :items="countRooms"/>
+        </v-col>
+          </v-row>
+                <v-textarea
+                class="mb-5"
+                label="Description">
+            </v-textarea>
+             <v-row>
+                <v-col
+                cols="12"
+                md="6">
+                    <v-checkbox
+                    class="mb-5"
+                    label="pool"/>
+                </v-col>
+                <v-col
+                cols="12"
+                md="6">
+                    <v-checkbox
+                    class="mb-5"
+                    label="yard"/>
+                </v-col>
+        </v-row>
+        <v-btn
+        block
+        color="grey">
+            add propertie
+        </v-btn>
+  </v-form>
+    </v-card>
 </template>
