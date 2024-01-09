cd8a99cd13bfdcf34bc7304b17043d6917fc5863
illocarlos <carlosalbendiz93@gmail.com>
2024-01-09 23:15:06 +0100

    validate

--- src/views/admin/CreateView.vue
+++ src/views/admin/CreateView.vue
@@ -9,6 +9,12 @@ const { handleSubmit } = useForm({
     }
 })
 const countRooms = [1, 2, 3, 4, 5, 6, 7, 8];
+const title = useField('title')
+const photo = useField('photo')
+const wc = useField('wc')
+const price = useField('price')
+const rooms = useField('rooms')
+const description = useField('description')
 
 const submit = handleSubmit((values) => {
   
@@ -29,18 +35,23 @@ tag="h2">
 <v-text-field
 class="mb-5"
 label="Title of propertie"
-v-model="title.alue.value"
-:error-message="title.errorMessage.value"/>
+v-model="title.value.value"
+:error-messages="title.errorMessage.value"/>
 
 <v-file-input
 accept="image/jpeg"
 label="photo"
 prepend-icon="mdi-camera"
 class="mb-5"
+    v-model="photo.value.value"
+    :error-messages="photo.errorMessage.value"
 />
     <v-text-field
     class="mb-5"
-    label="Price"/>
+    label="Price"
+     v-model="price.value.value"
+        :error-messages="price.errorMessage.value"
+    />
 
 
     <v-row>
@@ -51,7 +62,9 @@ md="4"
     <v-select
     class="mb-5"
     label="available rooms"
-    :items="countRooms"/>
+    :items="countRooms"
+     v-model="rooms.value.value"
+            :error-messages="rooms.errorMessage.value"/>
 </v-col>
 <v-col
     cols="12"
@@ -60,7 +73,9 @@ md="4"
       <v-select
         class="mb-5"
         label="WC"
-        :items="countRooms"/>
+        :items="countRooms"
+         v-model="wc.value.value"
+            :error-messages="wc.errorMessage.value"/>
         </v-col>
         <v-col
         cols="12"
@@ -74,7 +89,9 @@ md="4"
           </v-row>
                 <v-textarea
                 class="mb-5"
-                label="Description">
+                label="Description"
+                v-model="description.value.value"
+                :error-messages="description.errorMessage.value">
             </v-textarea>
              <v-row>
                 <v-col
