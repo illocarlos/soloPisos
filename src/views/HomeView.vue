88772241d0d382e08845344cae2ac376754c2096
illocarlos <carlosalbendiz93@gmail.com>
2024-01-11 22:58:14 +0100

    get api countries store countries helper formatphone form properties 2.0

--- src/views/HomeView.vue
+++
@@ -1,7 +0,0 @@
-<script setup>
-
-</script>
-
-<template>
-   <h1 class="text-center text-h1">DESDE HOME</h1>
-</template>
--- src/views/admin/EditView.vue
+++
@@ -1,7 +0,0 @@
-<script setup>
-
-</script>
-
-<template>
-  <h2 class="text-center text-h3 my-5 font-weight-bold">EDIT</h2>
-</template>
